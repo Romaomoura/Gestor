4cbdad341b22c509a46cd6757f27045ff7fe4049
Romaomoura <c.romaomoura@gmail.com>
2021-12-09 20:34:32 -0300

    Ajustes de CORS e Construtores DTO

--- src/main/java/com/romamoura/gestorfinanceiro/GestorfinanceiroApplication.java
+++ src/main/java/com/romamoura/gestorfinanceiro/GestorfinanceiroApplication.java
@@ -2,10 +2,19 @@ package com.romamoura.gestorfinanceiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
-public class GestorfinanceiroApplication {
+@EnableWebMvc
+public class GestorfinanceiroApplication implements WebMvcConfigurer{
+
+	@Override
+	public void addCorsMappings( CorsRegistry registry ){
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestorfinanceiroApplication.class, args);
--- src/main/java/com/romamoura/gestorfinanceiro/api/dto/LancamentoDTO.java
+++ src/main/java/com/romamoura/gestorfinanceiro/api/dto/LancamentoDTO.java
@@ -2,11 +2,15 @@ package com.romamoura.gestorfinanceiro.api.dto;
 
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LancamentoDTO {
     
     private Long id;
