229f1795703c5bcce8e84535b437e15c30a8a6d7
Romaomoura <c.romaomoura@gmail.com>
2022-03-15 19:45:49 -0300

    Ajustes TokenDTO

--- src/main/java/com/romamoura/gestorfinanceiro/api/dto/TokenDTO.java
+++ src/main/java/com/romamoura/gestorfinanceiro/api/dto/TokenDTO.java
@@ -1,12 +1,14 @@
 package com.romamoura.gestorfinanceiro.api.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class TokenDTO {
 
     private String nome;
